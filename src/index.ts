d4c1b918f9dc3f62b43cdfa830fe5e7907729d41
subject026 <subject026@protonmail.com>
2023-11-05 10:09:28 +0000

    add some code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+export function waaaa() {
+  return "waaaaa";
+}
+
+/**
+ * Nice function for saying heyy
+ * @returns {"heyy"}
+ */
 export function heyy() {
   return "heyy";
 }
 
-export function waaaa() {
-  return "heyy";
+export function adder(a: number, b: number): number {
+  return a + b;
 }
