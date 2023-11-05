78c8d23430d0b6f9c1a00f6cf5ec314301fcfe67
subject026 <subject026@protonmail.com>
2023-11-05 11:07:06 +0000

    update some code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
+/**
+ * high quality scream string
+ * @returns {"waaaaa"}
+ */
 export function waaaa() {
   return "waaaaa";
 }
 
 /**
- * Nice function for saying heyy
+ * nice function for saying heyy
  * @returns {"heyy"}
  */
 export function heyy() {
   return "heyy";
 }
 
+/**
+ * rather nicely adds 2 numbers rather nicely
+ * @param a
+ * @param b
+ * @returns {number}
+ */
 export function adder(a: number, b: number): number {
   return a + b;
 }
